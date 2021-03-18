244666acf362ce7057b9b6699c7d512aa8c730e9
EmmanueleLibraro <emmanuelelibraro95@gmail.com>
2021-03-18 15:56:58 +0100

    richiesta cognome

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,15 @@
+// RICHIESTA NOME 
 var name = prompt('Come ti chiami?');
 
 console.log(name);
-console.log(typeof(name));
+console.log( typeof(name));
 
 document.getElementById('main-title').innerHTML = 'Ciao ' + name;
+
+//RICHIESTA COGNOME
+var surname = prompt('Il tuo cognome?');
+
+console.log(surname);
+console.log( typeof(surname));
+
+document.getElementById('main-title').innerHTML = 'Ciao ' + surname;
