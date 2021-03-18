e0ef39ef9e6213711c4e85c8b922b64033c8e2b9
EmmanueleLibraro <emmanuelelibraro95@gmail.com>
2021-03-18 16:10:05 +0100

    fine

--- javascript/script.js
+++ javascript/script.js
@@ -21,3 +21,11 @@ console.log(color);
 console.log( typeof(color));
 
 document.getElementById('colore').innerHTML = 'Il mio colore preferito è ' + color;
+
+//RICHIESTA DI NOME-COGNOME-COLORE+NUMERO
+var all = prompt('La tua password');
+
+console.log(all);
+console.log( typeof(all) );
+
+document.getElementById('password').innerHTML = 'Inserisci la tua password ' + name + surname + color + 21;
