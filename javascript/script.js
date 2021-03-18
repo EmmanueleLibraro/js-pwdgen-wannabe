d715fcdbf4bbe7a1a34e48d45edd978b0db31830
EmmanueleLibraro <emmanuelelibraro95@gmail.com>
2021-03-18 16:01:40 +0100

    colore preferito

--- javascript/script.js
+++ javascript/script.js
@@ -12,4 +12,12 @@ var surname = prompt('Il tuo cognome?');
 console.log(surname);
 console.log( typeof(surname));
 
-document.getElementById('main-title').innerHTML = 'Ciao ' + surname;
+document.getElementById('second-title').innerHTML = 'Il mio cognome è ' + surname;
+
+//RICHIESTA COLORE PREFERITO
+var color = prompt('Colore preferito?');
+
+console.log(color);
+console.log( typeof(color));
+
+document.getElementById('colore').innerHTML = 'Il mio colore preferito è ' + color;
